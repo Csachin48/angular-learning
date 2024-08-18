92d16367f0ccb7bd7606de495e69ac6b902b828f
Sachin Chauhan <csachinj@gmail.com>
2024-08-19 03:34:50 +0530

    #Update url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./module/auth/auth.module').then(m => m.AuthModule) },
-  // Add other routes here
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
